62cf6375975245d7373d1dec7bf023e45dbe00d5
Punsara Rajapaksa <punsararajapaksa@gmail.com>
2026-03-06 00:49:04 +0530

    Add UserValidator for age validation

---
+++ com/concurrentuser/UserValidator.java
@@ -0,0 +1,18 @@
+package com.concurrentuser;
+
+import java.util.ArrayList;
+import java.util.List;
+// UserValidator.java
+public class UserValidator {
+    public static boolean isValidAge(int age) {
+        return age > 0;
+    }
+    // Add other validation methods as needed
+}
+
+
+
+
+
+
+
